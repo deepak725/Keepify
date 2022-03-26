df25570f35bdec494fbfae1a3e5de50711aa7335
Harshit <202112091@daiict.ac.in>
2022-03-27 00:37:35 +0530

    second commit

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import './App.css'; 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
+import {Dummy} from "./components/dummy.component";
 import Login from "./components/login.component";
+
 import SignUp from "./components/signup.component";
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
     <div className="App">
       <nav className="navbar navbar-expand-lg navbar-light fixed-top">
         <div className="container">
-          <Link className="navbar-brand" to={"/sign-in"}>Project</Link>
+          <Link className="navbar-brand" to={"/sign-in"}>Keepify</Link>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
@@ -28,7 +29,7 @@ function App() {
       <div className="outer">
         <div className="inner">
           <Switch>
-            <Route exact path='/' component={Login} />
+            <Route exact path='/' component={Login} /> 
             <Route path="/sign-in" component={Login} />
             <Route path="/sign-up" component={SignUp} />
           </Switch>
--- Frontend/src/components/login.component.js
+++ Frontend/src/components/login.component.js
@@ -1,7 +1,34 @@
-import React, { Component } from "react";
+import { message } from "antd";
+import React, { useState,useEffect } from "react";
+import { connect } from "react-redux";
+import  {loginRequest} from "../store/Reducers/login";
+import {Dummy} from "./dummy.component";
+// import { Route } from "react-router-dom";
+import {  Route, useHistory } from "react-router-dom";
 
-export default class Login extends Component {
-    render() {
+const Login =  (props) => {
+    const [password,setPassword] = useState("");
+    const [email,setEmail] = useState("");
+    const history = useHistory();
+    useEffect(() =>{
+         
+        if(props.status == 200){
+        history.push(`/dashboard`);
+        // <Route exact path='/dashboard' component={Dummy} /> 
+
+        }
+        
+    },[props.token])
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let values= {password,email};
+        if(password == "" || email == ""){
+            message.error("Fill the fields properly");
+            return
+        };
+       
+        props.loginRequest(values);
+    }
         return (
             <form>
 
@@ -9,24 +36,30 @@ export default class Login extends Component {
 
                 <div className="form-group">
                     <label>Email</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <input type="email" value = {email}
+                     onChange = {(e) => setEmail(e.target.value)} className="form-control" placeholder="Enter email" />
                 </div>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <input type="password" value = {password}
+                     onChange = {(e) => setPassword(e.target.value)} className="form-control" placeholder="Enter password" />
                 </div>
 
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                    </div>
-                </div>
+               
 
-                <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in</button>
+                <button type="submit" onClick={(e) => handleSubmit(e)} className="btn btn-dark btn-lg btn-block">Sign in</button>
                
             </form>
         );
     }
-}
+
+    const mapDispatchToProps = {
+        loginRequest,
+      }
+      const mapStateToProps = (login) => ({
+        status:login.status,
+        token:login.token
+    });
+    
+      export default connect(mapStateToProps,mapDispatchToProps)(Login);
