f87a250d9b32d60bd9bc51ce53dbaaf27235d70a
Harshit <202112091@daiict.ac.in>
2022-03-27 21:40:54 +0530

    login-signup

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css'; 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import {Dummy} from "./components/dummy.component";
+import { BrowserRouter, Router, Route, Link } from "react-router-dom";
 import Login from "./components/login.component";
-
+import {Routes} from "react-router-dom";
 import SignUp from "./components/signup.component";
 
 function App() {
-  return (<Router>
+  return (
     <div className="App">
       <nav className="navbar navbar-expand-lg navbar-light fixed-top">
         <div className="container">
@@ -16,10 +15,10 @@ function App() {
           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                <Link className="nav-link" to={"/sign-in"}>Sign in</Link>
+                <Link className="nav-link" to={"/signin"}>Sign in</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+                <Link className="nav-link" to={"/signup"}>Sign up</Link>
               </li>
             </ul>
           </div>
@@ -28,14 +27,14 @@ function App() {
 
       <div className="outer">
         <div className="inner">
-          <Switch>
-            <Route exact path='/' component={Login} /> 
-            <Route path="/sign-in" component={Login} />
-            <Route path="/sign-up" component={SignUp} />
-          </Switch>
+          <Routes>
+            <Route exact path='/' element={<Login/>} /> 
+            <Route path="/signin" element={<Login/>} />
+            <Route path="/signup" element={<SignUp/>} />
+          </Routes>
         </div>
       </div>
-    </div></Router>
+    </div>
   );
 }
 
